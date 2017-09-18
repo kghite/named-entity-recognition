4b669f348d7d0ed303af783da8e77a35025d6fc3
Andrew Deaver <deaver.andrew@gmail.com>
2017-09-18 19:59:59 -0400

    added data reading

---
+++ data_util.py
@@ -0,0 +1,46 @@
+class Reader():
+    def __init__(self, filename):
+        self.filename = "./corpus/" + filename
+        self.IGNORE_WORD = "-X-"
+
+    def process_words(self):
+        f = open(self.filename, "r+")
+        words = []
+        sentences = []
+        for line in f.readlines():
+            if len(line.strip()) > 0:
+                word = self.process_line(line)
+                if word.pos != self.IGNORE_WORD:
+                    words.append(word)
+            else:
+                if len(words) > 0:
+                    sentences.append(words)
+                    words = []
+        if len(words) > 0:
+            sentences.append(words)
+        return sentences
+
+    def process_line(self, line):
+        parts = line.strip().split()
+        if len(parts) == 4:
+            return TaggedWord(parts[0], parts[1], parts[2], parts[3])
+        return TaggedWord("", self.IGNORE_WORD, "", "") 
+
+class TaggedWord():
+    def __init__(self, word, pos, phrase, tag):
+        self.word = word
+        self.pos = pos
+        self.phrase = phrase
+        self.tag = tag.split("-")[1] if tag.startswith("I-") else tag
+
+    def __str__(self):
+        return "({}, {}, {}, {})".format(self.word, self.pos, self.phrase, self.tag)
+
+# EXAMPLE
+'''
+r = Reader("eng.train")
+lines = r.process_words()
+for line in lines:
+    for word in line:
+        print word
+'''
